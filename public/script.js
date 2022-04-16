ccd4b1a448e8d52ded5bdaf968309da914b20da0
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-04-16 16:49:09 +0700

    init firebase

--- public/script.js
+++ public/script.js
@@ -1,7 +1,23 @@
-document.getElementById("professor-search").onchange = () => {
+import { initializeApp} from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc , getDoc} from "firebase/firestore"; 
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB6ssZ2hOfOx-UoUJEVGBjVJd7pQylAtLM",
+  authDomain: "cp-rate-my-professor.firebaseapp.com",
+  projectId: "cp-rate-my-professor",
+  storageBucket: "cp-rate-my-professor.appspot.com",
+  messagingSenderId: "967764561714",
+  appId: "1:967764561714:web:e1a728b40b7cb3265e512b",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+document.getElementById("professor-search").onchange = async () => {
   const searchQuery = document.getElementById("professor-search").value;
 
   console.log(searchQuery);
-
+  const docRef = await getDoc(collection(db, "professor-names"))
+  console.log(docRef)
 };
