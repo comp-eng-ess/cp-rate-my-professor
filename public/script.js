724008702e4a58721bef25520c5f317636798db8
Gavin <yod.ittipat@gmail.com>
2022-04-23 11:30:52 +0700

    responsive top nav bar

--- public/script.js
+++ public/script.js
@@ -178,6 +178,14 @@ const genCommentPage = (id, professorName) => {
   };
 };
 
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const mobileNav = document.getElementsByClassName('mobile-nav')[0];
+
+toggleButton.addEventListener('click', () => {
+  mobileNav.classList.toggle('active')
+});
+
+
 document.getElementById("go-home").onclick = genHomePage;
 document.getElementById("logo").onclick = genHomePage;
 document.getElementById("go-contact").onclick = genContactPage;
